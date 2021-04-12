19dbfd90051af0258316f1f26d29d08ee1b40774
pmckernin <patrick@firstdraft.com>
2021-04-12 20:47:40 +0000

    changed routes back

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
- root "movies#index" 
+  get("/", { :controller => "movies", :action => "index" })
 
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create" 
-  get "/movies/new" => "movies#new" 
+  post("/movies", { :controller => "movies", :action => "create" })
+  get("/movies/new", { :controller => "movies", :action => "new" })
           
   # READ
-  get "/movies" => "movies#index" 
-  get "/movies/:id" => "movies#show" 
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:id", { :controller => "movies", :action => "show" })
   
   # UPDATE
-  patch "/movies/:id" => "movies#update" 
-  get "/movies/:id/edit" => "movies#edit" 
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
+  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
   
   # DELETE
-  delete "/movies/:id" => "movies#destroy" 
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
 
   #------------------------------
 end
