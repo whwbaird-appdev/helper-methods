aae71d12f82a5c2451c927c919d3849a98f21354
whw-baird <wbaird@chicagobooth.edu>
2022-04-14 01:35:31 +0000

    More refactoring

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
-
-    render template: "movies/new.html.erb"
   end
 
   def index
@@ -15,9 +13,7 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index.html.erb"
-      end
+      format.html
     end
   end
 
@@ -27,8 +23,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template: "movies/show.html.erb"
   end
 
   def create
@@ -40,7 +34,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new.html.erb"
+      render "new"
     end
   end
 
@@ -50,8 +44,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template: "movies/edit.html.erb"
   end
 
   def update
