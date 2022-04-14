c020676edf15ec0e5fb08d42aca44fdb36e77fa5
whw-baird <wbaird@chicagobooth.edu>
2022-04-14 01:29:47 +0000

    Cleaned up movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render({ :template => "movies/index.html.erb" })
+        render template: "movies/index.html.erb"
       end
     end
   end
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/show.html.erb" })
+    render template: "movies/show.html.erb"
   end
 
   def create
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to movies_url, notice: "Movie created successfully."
     else
       render template: "movies/new.html.erb"
     end
@@ -51,7 +51,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/edit.html.erb" })
+    render template: "movies/edit.html.erb"
   end
 
   def update
@@ -63,9 +63,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movie_url(the_movie), notice: "Movie updated successfully."
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movie_url(the_movie), alert: "Movie failed to update successfully."
     end
   end
 
@@ -75,6 +75,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
